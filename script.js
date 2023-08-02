cb6882a1e18934a0429e43cb73ec7bb630254ab2
Viktor Obedkov <obedkov.2018@yandex.ru>
2023-08-02 20:14:04 +0300

    Обработал функции  лайк

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const nameInput = document.querySelector('.popup__input[name="name"]');
 const jobInput = document.querySelector('.popup__input[name="about"]');
 const addButton = document.querySelector('.profile__add-button');
 const template = document.querySelector('#elements');
+
 const initialCards = [
     {
         name: 'Архыз',
@@ -35,7 +36,7 @@ const initialCards = [
 ];
 
 
-function renderCard({name, link}){
+function renderCard({name, link}) {
     const newElement = template.content.cloneNode(true);
     const image = newElement.querySelector('.element__image');
     image.src = link;
@@ -54,11 +55,8 @@ editButton.addEventListener('click', function () {
     jobInput.value = document.querySelector('.profile__subtitle').textContent;
     popupEdit.classList.add('popup_opened');
 });
-addButton.addEventListener('click', function (){
+addButton.addEventListener('click', function () {
     popupAdd.classList.add('popup_opened');
-});
-closeButton.addEventListener('click', function closePopup() {
-    popupEdit.classList.remove("popup_opened");
 });
 
 closeButton.forEach(button => {
@@ -87,4 +85,39 @@ function handleFormSubmit(evt) {
     jobOutput.textContent = jobValue;
     closePopup();
 }
+
 formElement.addEventListener('submit', handleFormSubmit);
+
+function addElement(evt) {
+    evt.preventDefault();
+    const elementTitle = document.querySelector('#title').value;
+    const elementLink = document.querySelector('#link').value;
+    const newCard = createCard(elementTitle, elementLink);
+    const cardsContainer = document.querySelector('.elements__list');
+    cardsContainer.prepend(newCard);
+    closePopup();
+}
+
+function createCard(title, link) {
+    const template = document.querySelector('#elements');
+    const cardElement = template.content.cloneNode(true);
+    const card = cardElement.querySelector('.element');
+    const cardImage = cardElement.querySelector('.element__image');
+    const cardTitle = cardElement.querySelector('.element__subtitle');
+    const cardLike = cardElement.querySelector('.element__like');
+
+    cardImage.src = link;
+    cardTitle.textContent = title;
+    cardLike.addEventListener('click', function () {
+        cardLike.classList.toggle('element__like_active');
+    });
+    return cardElement;
+}
+
+popupAdd.addEventListener('submit', addElement);
+
+
+const likeDisable = document.querySelector('.element__like');
+likeDisable.addEventListener('click', function () {
+    likeDisable.classList.toggle('element__like_active');
+});
