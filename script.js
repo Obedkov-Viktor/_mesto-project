9f31f85357faf452a092cb43e8fbfbab18de4a80
Viktor Obedkov <obedkov.2018@yandex.ru>
2023-08-14 15:22:13 +0300

    js проблема массив

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+import {initialCards} from "./cards";
+
+const popupImage = document.querySelector('.popup-picture');
+const addProfileButton = document.querySelector('.profile__add-button'); //кнопка "Добавление профиль"
+const editProfileButton = document.querySelector('.profile__edit-button');//кнопка "Редактировать"
+const popupAdd = document.querySelector('.popup-add'); //модальное кнопка для добавление картинки и ссылки
+const popupEdit = document.querySelector('.popup-edit');//Модальное кнопка для редактирование профиль
+const popupCloseButton = document.querySelector('.popup__close-button'); // Кнопка закрытия  модального окно
+
 
 function renderCard({name, link}) {
     const template = document.querySelector('#elements');
@@ -46,9 +29,9 @@ function renderCard({name, link}) {
 
     ///////// обработчик события клика на изображение ///////////
 
-    imageElement.addEventListener('click', function (){
-        const popupImage = document.querySelector('.popup-picture');
-        const popupCloseButton = popupImage.querySelector('.popup__close-button');
+    imageElement.addEventListener('click', function () {
+
+        const imagePopupCloseButton = popupImage.querySelector('.popup__close-button');
         const popupImageElement = popupImage.querySelector('.popup__image');
         const popupCaption = popupImage.querySelector('.popup__caption');
 
@@ -57,20 +40,13 @@ function renderCard({name, link}) {
         popupCaption.textContent = name;
         openModal(popupImage);
 
-        popupCloseButton.addEventListener('click', function (){
+        imagePopupCloseButton.addEventListener('click', function () {
             closeModal(popupImage);
         })
     })
 
     return list.appendChild(card);
 }
-
-const addProfileButton = document.querySelector('.profile__add-button'); //кнопка "Добавление профиль"
-const editProfileButton = document.querySelector('.profile__edit-button');//кнопка "Редактировать"
-const popupAdd = document.querySelector('.popup-add'); //модальное кнопка для добавление картинки и ссылки
-const popupEdit = document.querySelector('.popup-edit');//Модальное кнопка для редактирование профиль
-const popupCloseButton = document.querySelector('.popup__close-button'); // Кнопка закрытия  модального окно
-// const popupImage = document.querySelector('.popup-picture');//Модальное кнопка для октрытая фотография
 
 /////////////////    Открыть модального окно    /////////////////////
 function openModal(popup) {
@@ -94,7 +70,7 @@ editProfileButton.addEventListener('click', () => {
     openModal(popupEdit);
 })
 
-initialCards.forEach(renderCard);
+initialCards.reverse().forEach(renderCard);
 
 ///////////////    Редактирование профиль    //////////////////
 
@@ -138,7 +114,7 @@ popupForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const titleInput = popupForm.querySelector('.popup__input[name="title"]').value;
     const imageInput = popupForm.querySelector('.popup__input[name="link"]').value;
-    renderCard({name:titleInput, link:imageInput});
+    renderCard({name: titleInput, link: imageInput});
     closeModal();
 });
 
